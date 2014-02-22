5d5ae657eb2260df7eb336e55eb9d29589a0f226
Vlad Andersen <vlad.andersen@gmail.com>
2014-02-22 04:12:30 +0400

    Initial grumpy commit.

--- grumpy.gemspec
+++ grumpy.gemspec
@@ -18,6 +18,10 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "railties"
+  spec.add_dependency "sass-rails"
+  spec.add_dependency "sprockets"
+
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
 end
--- lib/grumpy.rb
+++ lib/grumpy.rb
@@ -1,5 +1,11 @@
 require "grumpy/version"
 
 module Grumpy
-  # Your code goes here...
+  module Rails
+    class Engine < ::Rails::Engine
+      initializer :after_append_asset_paths, :group => :all, :after => :append_assets_path do |app|
+        app.config.assets.paths << File.expand_path('../app/assets/stylesheets/grumpy.css.scss', __FILE__)
+      end
+    end
+  end
 end
